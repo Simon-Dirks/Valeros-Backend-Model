3725a797ecabd2896a9e1e4b761c03bd7e0a6422
Simon Dirks <mail@simondirks.com>
2026-02-09 12:14:02 +0100

    Add initial search response data model

--- search/search-response.ts
+++ search/search-response.ts
@@ -1,3 +1,57 @@
 /** @format */
 
-// TODO
+type NodeLabel = {
+  value: string;
+  language: string;
+};
+type NodeObj = {
+  value: string;
+  label?: NodeLabel[];
+  direction: "outgoing" | "incoming";
+};
+type NodeModel = {
+  id: string;
+  datasetRegisterIds: string[];
+  data: { [predicate: string]: NodeObj[] };
+};
+
+type SearchQueryResponseModel = NodeModel[];
+
+const response: SearchQueryResponseModel = [
+  {
+    id: "https://data.razu.nl/id/object/nl-wbdrazu-k50907905-689-2387",
+    datasetRegisterIds: [
+      "http://data.bibliotheken.nl/id/dataset/dbnlt",
+      "https://data.spinque.com/ld/data/vangoghworldwide/albertina/",
+    ],
+    data: {
+      "http://purl.org/dc/terms/title": [
+        {
+          value: "...",
+          label: [
+            {
+              value: "...",
+              language: "nl",
+            },
+            {
+              value: "...",
+              language: "en",
+            },
+          ],
+          direction: "outgoing",
+        },
+        {
+          value: "...",
+          label: [
+            {
+              value: "...",
+              language: "nl",
+            },
+          ],
+          direction: "outgoing",
+        },
+      ],
+    },
+    // ...
+  },
+];
